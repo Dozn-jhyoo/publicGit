d26a400ad4ecd3d899502e7a9cd676eee2e1c597
EXT_JHYOO_PC\jhyoo <jhyoo@dozn.co.kr>
2021-05-20 17:24:12 +0900

    pub-1   heon3

--- PublicTest/Form1.Designer.cs
+++ PublicTest/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace PublicTest
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -51,11 +52,21 @@ namespace PublicTest
             this.button2.Text = "heon2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(542, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "heon3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -68,6 +79,7 @@ namespace PublicTest
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
