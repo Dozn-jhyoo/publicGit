e39072ad40afc0e1cf000e51760981e8b688b1b8
chlim <chlim@dozn.co.kr>
2021-05-20 17:58:02 +0900

    lim1

--- PublicTest/Form1.Designer.cs
+++ PublicTest/Form1.Designer.cs
@@ -33,6 +33,7 @@ namespace PublicTest
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -71,11 +72,21 @@ namespace PublicTest
             this.button4.Text = "PUB-2";
             this.button4.UseVisualStyleBackColor = true;
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(542, 273);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 41);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "lim1";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -92,6 +103,7 @@ namespace PublicTest
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
