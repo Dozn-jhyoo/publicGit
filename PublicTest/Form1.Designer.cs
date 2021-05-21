fdc2be53d48ac80efed2dea10f6a072bc112b54d
EXT_JHYOO_PC\jhyoo <jhyoo@dozn.co.kr>
2021-05-21 20:57:20 +0900

    배포

--- PublicTest/Form1.Designer.cs
+++ PublicTest/Form1.Designer.cs
@@ -42,6 +42,7 @@ namespace PublicTest
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -165,11 +166,21 @@ namespace PublicTest
             this.button9.Text = "룰2 적용";
             this.button9.UseVisualStyleBackColor = true;
             // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(145, 362);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(104, 41);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "배포";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.checkBox4);
@@ -205,6 +216,7 @@ namespace PublicTest
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
 
