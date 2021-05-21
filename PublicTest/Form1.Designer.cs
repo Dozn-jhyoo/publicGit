420983e36b768f370913a428840f7c7535e9eab1
EXT_JHYOO_PC\jhyoo <jhyoo@dozn.co.kr>
2021-05-21 13:53:15 +0900

    long

--- PublicTest/Form1.Designer.cs
+++ PublicTest/Form1.Designer.cs
@@ -37,6 +37,7 @@ namespace PublicTest
             this.button6 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -113,11 +114,21 @@ namespace PublicTest
             this.checkBox2.Text = "checkBox2";
             this.checkBox2.UseVisualStyleBackColor = true;
             // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(145, 170);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(104, 41);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "heon";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button6);
@@ -143,6 +154,7 @@ namespace PublicTest
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button7;
     }
 }
 
